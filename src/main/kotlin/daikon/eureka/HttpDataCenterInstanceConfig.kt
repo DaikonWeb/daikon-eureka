94d741fc239bd5d50f3a8825ad882894392a6565
Alessio Coser <alessio.coser@xpeppers.com>
2020-04-09 11:37:52 +0200

    updated daikon dependency

--- src/main/kotlin/daikon/eureka/HttpDataCenterInstanceConfig.kt
+++ src/main/kotlin/daikon/eureka/HttpDataCenterInstanceConfig.kt
@@ -2,14 +2,15 @@ package daikon.eureka
 
 import com.netflix.appinfo.MyDataCenterInstanceConfig
 import daikon.core.Context
+import daikon.port
 
 class HttpDataCenterInstanceConfig(namespace: String, private val context: Context) : MyDataCenterInstanceConfig(namespace) {
 
     override fun getSecurePort(): Int {
-        return context.getAttribute("port")
+        return context.port()
     }
 
     override fun getNonSecurePort(): Int {
-        return context.getAttribute("port")
+        return context.port()
     }
 }
